d5cb91941d77be3a3bfc4292c489241aa3b895b2
Maksym Pakula <gargantadoto@gmail.com>
2025-07-26 19:38:29 +0300

    fix return type of wrapper

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from functools import wraps
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     results = {}
 
     @wraps(func)
-    def wrapper(*args, **kwargs) -> None:
+    def wrapper(*args, **kwargs) -> Any:
         if args not in results:
             results[args] = func(*args, **kwargs)
             print("Calculating new result")
